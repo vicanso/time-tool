007c63654c425ed2e9ad83557f8fe630ad7261cd
vicanso <vicansocanbico@gmail.com>
2016-12-01 22:04:59 +0800

    modify the ns time value's calculation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const now = Date.now();
 const start = process.hrtime();
 const currentSeconds = Math.floor(now / 1000);
 const currentUs = (now % 1000) * 1000 * 1000;
-
+const tenNS = 10 * 1000 * 1000 * 100;
 function pad(value, length) {
   const str = '00000000000000000'.substring(0, length);
   const v = '' + value;
@@ -11,8 +11,12 @@ function pad(value, length) {
 
 function ns(v) {
   const arr = process.hrtime(start);
-  const ns = pad(`${arr[1] + currentUs}`, 9);
-  return `${currentSeconds + arr[0]}${ns}`;
+
+  const ns = arr[1] + currentUs;
+  if (ns >= tenNS) {
+    return `${currentSeconds + arr[0] + 1}${pad(ns - tenNS, 9)}`;
+  }
+  return `${currentSeconds + arr[0]}${pad(ns, 9)}`;
 }
 
 function us(v) {
